e6fa27e9a7433ae0d962f12cee6fc08f925cdff9
Pritiraj Partho <dpartho5616@gmail.com>
2024-10-25 21:04:25 +0600

    second adding

--- app/page.js
+++ app/page.js
@@ -3,6 +3,7 @@ export default function Home() {
   return (
     <div className="flex justify-center items-center max-h-screen">
        <h1 className=" text-red-500  text-4xl mt-9">We want to make a World best Software Company</h1>
+       <h3 className="text-center text-green-500">With prper dedication</h3>
     </div>
   );
 }
